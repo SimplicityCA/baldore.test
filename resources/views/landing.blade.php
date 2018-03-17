13db46f7938997bdaf147fd7098fc4876bdb789e
Franklin Paula <frankpaul142@gmail.com>
2018-03-16 21:58:15 -0500

    fix text

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -28,7 +28,10 @@
                   </div>
                   <div class="mbn_ne">
                     <p style="text-align: center">
-                    Advertencia. El consumo excesivo de alcohol limita su capacidad de conducir y operar maquinarias, puede causar daños en su salud y perjudica a su familia”.<br> “Ministerio de Salud Pública del Ecuador”.<br> “Venta prohibida a menores de 18 años”.
+                       “Advertencia. El
+                      consumo excesivo de alcohol limita su capacidad de conducir y operar maquinarias, puede causar
+                      daños en su salud y perjudica a su familia”. “Ministerio de Salud Pública del Ecuador”. “Venta
+                      prohibida a menores de 18 años”. 
                     </p>
                   </div>
                 </div>
